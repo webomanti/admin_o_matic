c813f70213cb88adc491a00afa3dc3b800695cac
webomanti <webomanti@gmail.com>
2022-04-19 10:03:23 +0200

    Update SideBar.vue

--- resources/js/Components/SideBar.vue
+++ resources/js/Components/SideBar.vue
@@ -1,9 +1,9 @@
 <template>
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <inertia-link :href="route('admin.dashboard.index')" class="brand-link">
-            <img :src="'/storage/images/AdminLTELogo.png'" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">Admin-O-Matic</span>
+        <inertia-link :href="route('dashboard.index')" class="brand-link">
+            <img :src="'/images/AdminLTELogo.png'" alt="suzuki Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">Suzuki</span>
         </inertia-link>
 
         <!-- Sidebar -->
@@ -11,7 +11,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img :src="'/storage/images/user2-160x160.jpg'" class="img-circle elevation-2" alt="User Image">
+                    <img :src="'/images/user2-160x160.jpg'" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">{{ $page.props.user.name }}</a>
@@ -23,73 +23,56 @@
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library -->
-                    <li class="nav-item menu-open">
-                        <inertia-link :href="route('admin.dashboard.index')" class="nav-link" :class="route().current('admin.dashboard.*') ? 'active' : ' '">
+                    <li class="nav-item">
+                        <inertia-link :href="route('dashboard.index')" class="nav-link" :class="route().current('dashboard.*') ? 'active' : ' '">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
-                                Dashboard
+                                {{ $t('dashboard') }}
                             </p>
                         </inertia-link>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item has-treeview" :class="route().current('admin.*') ? ' menu-is-opening menu-open' : ' '">
                         <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-table"></i>
                             <p>
-                                Tables
+                                {{ $t('tables') }}
                                 <i class="fas fa-angle-left right"></i>
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item" v-if="$page.props.auth.hasRole.superAdmin || $page.props.auth.hasRole.admin || $page.props.auth.hasRole.moderator || $page.props.auth.hasRole.developer">
-                                <inertia-link :href="route('admin.admins.index')" class="nav-link" :class="route().current('admin.admins.*') ? 'active' : ' '">
+                                <inertia-link :href="route('admin.admins.index')" class="nav-link ml-2" :class="route().current('admin.admins.*') ? 'active' : ' '">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>
-                                        Admins
+                                        {{ $t('admins') }}
                                     </p>
                                 </inertia-link>
                             </li>
                             <li class="nav-item" v-if="$page.props.auth.hasRole.superAdmin || $page.props.auth.hasRole.admin || $page.props.auth.hasRole.moderator">
-                                <inertia-link :href="route('admin.users.index')" class="nav-link" :class="route().current('admin.users.*') ? 'active' : ' '">
+                                <inertia-link :href="route('admin.users.index')" class="nav-link ml-2" :class="route().current('admin.users.*') ? 'active' : ' '">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>
-                                        Users
+                                        {{ $t('users') }}
                                     </p>
                                 </inertia-link>
                             </li>
                             <li class="nav-item" v-if="$page.props.auth.hasRole.superAdmin || $page.props.auth.hasRole.admin || $page.props.auth.hasRole.moderator">
-                                <inertia-link :href="route('admin.roles.index')" class="nav-link" :class="route().current('admin.roles.*') ? 'active' : ' '">
+                                <inertia-link :href="route('admin.roles.index')" class="nav-link ml-2" :class="route().current('admin.roles.*') ? 'active' : ' '">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>
-                                        Roles
+                                        {{ $t('roles') }}
                                     </p>
                                 </inertia-link>
                             </li>
                             <li class="nav-item" v-if="$page.props.auth.hasRole.superAdmin || $page.props.auth.hasRole.admin">
-                                <inertia-link :href="route('admin.permissions.index')" class="nav-link" :class="route().current('admin.permissions.*') ? 'active' : ' '">
+                                <inertia-link :href="route('admin.permissions.index')" class="nav-link ml-2" :class="route().current('admin.permissions.*') ? 'active' : ' '">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>
-                                        Permissions
+                                        {{ $t('permissions') }}
                                     </p>
                                 </inertia-link>
                             </li>
                         </ul>
-                    </li>
-                    <li class="nav-header">MISCELLANEOUS</li>
-                    <li class="nav-item">
-                        <inertia-link :href="route('dashboard')" class="nav-link">
-                            <i class="nav-icon fas fa-home"></i>
-                            <p>
-                                Site
-                            </p>
-                        </inertia-link>
-                    </li>
-                    <li class="nav-item">
-                        <form @submit.prevent="logout">
-                            <button type="submit" class="nav-button">
-                                <i class="nav-icon fas fa-sign-out-alt"></i>
-                                <span>Logout</span>
-                            </button>
-                        </form>
                     </li>
                 </ul>
             </nav>
