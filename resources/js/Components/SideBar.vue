f901607684358a7b6c6f3fa649f82a021457facb
webomanti <webomanti@gmail.com>
2022-04-21 08:45:29 +0200

    Update SideBar.vue

--- resources/js/Components/SideBar.vue
+++ resources/js/Components/SideBar.vue
@@ -2,8 +2,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <inertia-link :href="route('dashboard.index')" class="brand-link">
-            <img :src="'/images/AdminLTELogo.png'" alt="suzuki Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">Suzuki</span>
+            <img :src="'/images/AdminLTELogo.png'" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">AdminLTE</span>
         </inertia-link>
 
         <!-- Sidebar -->
